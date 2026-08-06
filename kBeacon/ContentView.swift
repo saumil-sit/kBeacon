3976f6cfde26235c5a1c7edb5812c4e337d19ed6
Saumildoshi <saumil.doshi7@gmail.com>
2026-08-06 18:29:12 +0530

    Initial Commit

---
+++ kBeacon/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  kBeacon
+//
+//  Created by Saumil on 06/08/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ kBeacon/kBeaconApp.swift
@@ -0,0 +1,17 @@
+//
+//  kBeaconApp.swift
+//  kBeacon
+//
+//  Created by Saumil on 06/08/26.
+//
+
+import SwiftUI
+
+@main
+struct kBeaconApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
